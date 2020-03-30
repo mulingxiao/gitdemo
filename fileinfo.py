0901ed4e8aaee1e79a3dd748246c1677feebbe86
mulingxiao <18377037@buaa.edu.com>
2020-03-30 09:36:06 +0800

    updata by mulingxiao 2020

--- fileinfo.py
+++ fileinfo.py
@@ -6,3 +6,16 @@
 # Modifications      :
 
 # Description            : Show file information for a given file
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+score = int(raw_input('输入分数:\n'))
+if score >= 90:
+    grade = 'A'
+elif score >= 60:
+    grade = 'B'
+else:
+    grade = 'C'
+ 
+print '%d 属于 %s' % (score,grade)
